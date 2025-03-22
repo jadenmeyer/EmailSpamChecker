fe0095ccc502b29d8c2b9e3e4f194b44657d6a1a
jadenmeyer <44992067+jadenmeyer@users.noreply.github.com>
2025-03-21 22:01:32 -0500

    Working way of printing out messages from under header using pandas
    Signed-off-by: jadenmeyer <44992067+jadenmeyer@users.noreply.github.com>

--- dataCollection.py
+++ dataCollection.py
@@ -3,6 +3,10 @@ import nltk as nl
 import re
 
 def extract_email_body(email_text):
+    """
+    :param email_text: This is a column of the dataframe in which there is an email message
+    :return: a spliced version of the message where the header is cut out.
+    """
     email_body = re.search(r'\n\n', email_text) #assume newline after header is message
     if email_body:
         body = email_text[email_body.end():].strip() #splicer for characters after header text
@@ -16,6 +20,7 @@ email_dataframe = pd.read_csv('emails.csv')
 location_one = extract_email_body(email_dataframe.loc[1, 'message'])
 print(location_one)
 print(pd.options.display.max_rows)
+
 #print(email_dataframe)
 
 """Now to Preprocess the data"""
